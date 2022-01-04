9cdb6a5d362f0ba0c74ab9ef1757cc03e97924de
Duy Nguyen <duy210697@gmail.com>
2022-01-04 09:16:19 +0700

    fix error definition

--- common/error.js
+++ common/error.js
@@ -5,9 +5,14 @@ const { StatusCodes } = require('http-status-codes');
 const { ERROR } = require('../constants/error');
 
 class VError extends Error {
-    constructor({ err, message, code, errCode }) {
+    constructor({
+        message,
+        err = null,
+        code = StatusCodes.INTERNAL_SERVER_ERROR,
+        errCode = ERROR.INTERNAL_SERVER,
+    }) {
         let msg = message;
-        if (!(err instanceof Error)) {
+        if (err && !(err instanceof Error)) {
             msg = err;
             if (!msg) {
                 throw new Error('invalid error definition');
